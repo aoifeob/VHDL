f0695d6bfcf5cd9475f55610e88dca463e17fbb5
obriea21 <obriea21@tcd.ie>
2017-02-23 16:48:24 +0000

    Update nand_nor.vhd

--- nand_nor.vhd
+++ nand_nor.vhd
@@ -2,25 +2,25 @@
 ENTITY nand IS
   PORT( A1: in std_logic;
         A2: in std_logic;
-        AO: out std_logic;  );
+        AOut: out std_logic;  );
 END nand;
 
 ARCHITECTURE behavioural OF nand IS
 CONSTANT gate_delay: TIME:= 5ns
 BEGIN
-  AO <= (A1 NAND A2) after gate_delay; --NAND gate
+  AOut <= (A1 NAND A2) after gate_delay; --NAND gate
 END behavioural;
 
 --standard NOR
 ENTITY nor IS
-  PORT( O1: in std_logic;
-        O2: in std_logic;
-        OO: out std_logic;  );
+  PORT( B1: in std_logic;
+        B2: in std_logic;
+        BOut: out std_logic;  );
 END nor;
 
 ARCHITECTURE behavioural OF nor IS
 BEGIN
-  OO <= (O1 NOR O2); --NOR gate
+  BOut <= (B1 NOR B2); --NOR gate
 END behavioural;
 
 --fix for the i/o inversion on certain boards
@@ -31,24 +31,24 @@ USE ieee.std_logic_1164.all;
 ENTITY nand_nor IS
   PORT( A1: in std_logic;
         A2: in std_logic;
-        AO: out std_logic
-        O1: in std_logic;
-        O2: in std_logic;
-        OO: out std_logic );
+        AOut: out std_logic
+        B1: in std_logic;
+        B2: in std_logic;
+        BOut: out std_logic );
 END ;
 
 ARCHITECTURE behavioural OF nand_nor IS
-SIGNAL X1,X2,XO,Y1,Y2,YO: std_logic;
+SIGNAL X1,X2,XOut,Y1,Y2,YOut: std_logic;
 CONSTANT gate_delay: TIME:=5ns;
 BEGIN
-  XO <= (X1 NAND X2) after gate_delay; --NAND gate
-  YO <= (Y1 NOR Y2); --NOR gate
+  XOut <= (X1 NAND X2) after gate_delay; --NAND gate
+  YOut <= (Y1 NOR Y2); --NOR gate
   
   X1 <= NOT A1;
   X2 <= NOT A2;
-  AO <= NOT XO;
+  AOut <= NOT XOut;
   
-  Y1 <= NOT O1;
-  Y2 <= NOT O2;
-  OO <= NOT YO;
+  Y1 <= NOT B1;
+  Y2 <= NOT B2;
+  BOut <= NOT YOut;
 END behavioural;
