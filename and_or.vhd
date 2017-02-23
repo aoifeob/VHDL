7a6569d10d2ef5a05948602ab4bdbdbb65570c47
obriea21 <obriea21@tcd.ie>
2017-02-23 16:50:42 +0000

    Update and_or.vhd

--- and_or.vhd
+++ and_or.vhd
@@ -2,7 +2,7 @@
 ENTITY and IS
   PORT( A1: in std_logic;
         A2: in std_logic;
-        AO: out std_logic;  );
+        AOut: out std_logic  );
 END and;
 
 ARCHITECTURE behavioural OF and IS
@@ -13,14 +13,14 @@ END behavioural;
 
 --standard OR
 ENTITY or IS
-  PORT( O1: in std_logic;
-        O2: in std_logic;
-        OO: out std_logic;  );
+  PORT( B1: in std_logic;
+        B2: in std_logic;
+        BOut: out std_logic  );
 END or;
 
 ARCHITECTURE behavioural OF or IS
 BEGIN
-  OO <= (O1 OR O2); --OR gate
+  BOut <= (B1 OR B2); --OR gate
 END behavioural;
 
 --fix for the i/o inversion on certain boards
@@ -31,24 +31,24 @@ USE ieee.std_logic_1164.all;
 ENTITY and_or IS
   PORT( A1: in std_logic;
         A2: in std_logic;
-        AO: out std_logic
-        O1: in std_logic;
-        O2: in std_logic;
-        OO: out std_logic );
+        AOut: out std_logic
+        B1: in std_logic;
+        B2: in std_logic;
+        BOut: out std_logic );
 END and_or;
 
 ARCHITECTURE behavioural OF and_or IS
-SIGNAL X1,X2,XO,Y1,Y2,YO: std_logic;
+SIGNAL X1,X2,XOut,Y1,Y2,YOut: std_logic;
 CONSTANT gate_delay: TIME:=5ns;
 BEGIN
-  XO <= (X1 AND X2) after gate_delay; --AND gate
-  YO <= (Y1 OR Y2); --OR gate
+  XOut <= (X1 AND X2) after gate_delay; --AND gate
+  YOut <= (Y1 OR Y2); --OR gate
   
   X1 <= NOT A1;
   X2 <= NOT A2;
-  AO <= NOT XO;
+  AOut <= NOT XOut;
   
-  Y1 <= NOT O1;
-  Y2 <= NOT O2;
-  OO <= NOT YO;
+  Y1 <= NOT B1;
+  Y2 <= NOT B2;
+  BOut <= NOT YOut;
 END behavioural;
